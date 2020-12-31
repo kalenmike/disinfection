a91369699b4e64db5554f1e2951f0cc41a9ae5a2
Kalen Michael <kalenmike@gmail.com>
2020-12-31 17:01:58 +0100

    added service worker and pwa functionality

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open('sw-cache').then(function(cache){
+            return cache.addAll([
+                'index.html',
+                'manifest.json',
+                'dist/main.js',
+                'assets/*',
+                'https://fonts.googleapis.com/css?family=Baloo+2|Roboto&display=swap'
+            ]);
+        })
+    )
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            return response || fetch(event.request);
+        })
+    )
+});
