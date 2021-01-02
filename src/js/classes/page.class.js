9b26161773b5daeb02224f99b2363d6dde7a6a5d
Kalen Michael <kalenmike@gmail.com>
2021-01-02 20:25:18 +0100

    updated about

--- src/js/classes/page.class.js
+++ src/js/classes/page.class.js
@@ -56,9 +56,13 @@ export default class Page{
 
     buildAbout(){
         let frame = document.createElement('div');
-        frame.innerHTML = 
-        '<p>...I am still working on adding the about...</p>' +
-        '<h1>About</h1>';
+        frame.innerHTML =
+        '<h1>About</h1>' +
+        '<div class="scroll-content"><p>2020 the year so special they had to name it twice!</p>' + 
+        '<p>As we all sat locked in our houses, quaranteened and fearing for our futures we found ourselves with alot of extra time and very little to do.</p>' + 
+        '<p>I had recently moved to a new town and was living alone far away from friends and family. This game became my project. It changed many times over starting as a horror game and eventually becoming something more child friendly after I realised my nephews might enjoy playing it on their tablet.</p>' + 
+        'Unfortunately my original code was almost entirely lost as I only published my compliled code to my server. The realise of Ubunut 20.04 had me excited and I reformatted before realising I had not saved my local web projects. So what you see today is a reimagined version of what once was, and a tribute to all the time we spent indoors during the quarantine of 2020.' + 
+        '<p>I hope you enjoy playing my game!</p></div>';
 
         return frame;
     }
